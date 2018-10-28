fc0809421577cdb30461a33cc573ee38221ebce7
Aaron Lee <chinphang.lee@hotmail.com>
2018-10-28 19:17:04 +0800

    chore: hide visibility of page number

--- src/containers/People.jsx
+++ src/containers/People.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import queryString from 'query-string';
 import map from 'lodash/map';
+import cs from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChevronLeft,
@@ -15,6 +16,7 @@ import { fetchAllPeople } from '../reducers/actions';
 import { Loading } from '../components/Loading';
 import { peopleRoutes } from './routes';
 import { NotFound } from './NotFound';
+import { cx } from 'emotion';
 
 const mapStateToProps = state => ({
   data: state.peopleReducer.data || {},
@@ -70,17 +72,19 @@ export class PeopleContainer extends React.Component {
 
   render() {
     const qs = queryString.parse(this.props.location.search);
+    const isVisible = !this.props.loading && this.props.data.count;
 
     return (
       <div className={style.peopleContainer}>
         <div className={style.head}>
           <h1>All People in the Universe</h1>
-          {!this.props.loading &&
-            this.props.data.count && (
-              <h4 className={style.page}>
-                Page {qs.page} of {Math.ceil(this.props.data.count / 10)}
-              </h4>
-            )}
+          <h4
+            className={cx(`${style.page}`, {
+              [`${style.hidden}`]: !isVisible
+            })}
+          >
+            Page {qs.page} of {Math.ceil(this.props.data.count / 10)}
+          </h4>
         </div>
 
         <div className={style.people}>
