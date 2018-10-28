d8a85da38b25b444643af979e9c96ad5ab156f49
Aaron Lee <chinphang.lee@hotmail.com>
2018-10-29 05:55:29 +0800

    feat: async await promise all

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import map from 'lodash/map';
+import isEmpty from 'lodash/isEmpty';
 
 import * as style from './item.scss';
 
@@ -9,38 +11,88 @@ export class Item extends React.Component {
 
     return (
       <div className={style.detailContent}>
-        <p>
-          <span className={style.label}>Name: </span>
-          {people.name}
-        </p>
-        <p>
-          <span className={style.label}>Gender: </span>
-          {people.gender}
-        </p>
-        <p>
-          <span className={style.label}>Birth Year: </span>
-          {people.birth_year}
-        </p>
-        <p>
-          <span className={style.label}>Height: </span>
-          {people.height}
-        </p>
-        <p>
-          <span className={style.label}>Mass: </span>
-          {people.mass}
-        </p>
-        <p>
-          <span className={style.label}>Eye Color: </span>
-          {people.eye_color}
-        </p>
-        <p>
-          <span className={style.label}>Hair Color: </span>
-          {people.hair_color}
-        </p>
-        <p>
-          <span className={style.label}>Skin Color: </span>
-          {people.skin_color}
-        </p>
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Name: </span>
+              {people.name}
+            </p>
+            <p>
+              <span className={style.label}>Gender: </span>
+              {people.gender}
+            </p>
+            <p>
+              <span className={style.label}>Birth Year: </span>
+              {people.birth_year}
+            </p>
+            <p>
+              <span className={style.label}>Height: </span>
+              {people.height}
+            </p>
+          </div>
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Mass: </span>
+              {people.mass}
+            </p>
+            <p>
+              <span className={style.label}>Eye Color: </span>
+              {people.eye_color}
+            </p>
+            <p>
+              <span className={style.label}>Hair Color: </span>
+              {people.hair_color}
+            </p>
+            <p>
+              <span className={style.label}>Skin Color: </span>
+              {people.skin_color}
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Starships: </span>
+              {!isEmpty(people.starships)
+                ? map(people.starships, starship => <p>{starship.name}</p>)
+                : 'n/a'}
+            </p>
+          </div>
+
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Vehicles: </span>
+              {!isEmpty(people.vehicles)
+                ? map(people.vehicles, vehicle => <p>{vehicle.name}</p>)
+                : 'n/a'}
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Species: </span>
+              {map(people.species, species => (
+                <p>{species.name}</p>
+              ))}
+            </p>
+            <p>
+              <span className={style.label}>Homeworld: </span>
+              {homeworld.name}
+            </p>
+          </div>
+
+          <div className="col-md-6 text-center">
+            <p>
+              <span className={style.label}>Films: </span>
+              {map(people.films, film => (
+                <p>{film.name}</p>
+              ))}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/reducers/actions.js
+++ src/reducers/actions.js
@@ -1,3 +1,7 @@
+import map from 'lodash/map';
+import isEmpty from 'lodash/isEmpty';
+import reduce from 'lodash/reduce';
+
 import * as Types from './constants';
 import * as Request from '../utils/request';
 
@@ -15,13 +19,62 @@ const listError = error => ({
   payload: error
 });
 
+const fetchURLs = async urls => {
+  try {
+    let responses = await Promise.all(map(urls, Request.get));
+
+    return map(responses, res => res.data);
+  } catch (error) {
+    console.log('fetchURLs error: ', error);
+  }
+};
+
+const fetchURL = async url => {
+  try {
+    let response = await Request.get(url);
+
+    return response.data;
+  } catch (error) {
+    console.log('fetchURL error: ', error);
+  }
+};
+
+const mapPeople = results => {
+  return map(results, people => {
+    return reduce(
+      people,
+      (acc, value, key) => {
+        const arr = ['films', 'species', 'vehicles', 'starships'];
+
+        if (arr.includes(key) && !isEmpty(value)) {
+          fetchURLs(value).then(data => {
+            acc[key] = data;
+          });
+        } else if (key === 'homeworld') {
+          fetchURL(value).then(data => {
+            acc[key] = data;
+          });
+        } else {
+          acc[key] = value;
+        }
+
+        return acc;
+      },
+      {}
+    );
+  });
+};
+
 export const fetchAllPeople = url => async dispatch => {
   dispatch(listRequest());
 
   try {
     const response = await Request.get(url);
+    const mappedResults = mapPeople(response.data.results);
 
-    dispatch(listSuccess(response.data));
+    dispatch(
+      listSuccess(Object.assign(response.data, { results: mappedResults }))
+    );
   } catch (error) {
     dispatch(listError(error));
   }
